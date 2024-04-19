9f1315df677a5f1091f06193020e1171995fd4c6
connect2avni <smrutictc100@gmail.com>
2024-04-19 22:07:19 +0530

    queue

---
+++ dsapratice/QueueLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsapratice
+{
+    public class QueueLinkedList
+    {
+        public class Node
+        {
+            public int element;
+            public Node next;
+            public Node(int e, Node n)
+            {
+                element = e;
+                next = n;
+
+            }
+        }
+        class QueuesLinked
+        {
+            Node front;
+            Node rear;
+            int size;
+
+            public QueuesLinked()
+            {
+                front = null;
+                rear = null;
+                size = 0;
+            }
+            public int length()
+            
+            {
+                return size;
+            } 
+            public bool isEmpty()
+            {
+                return size == 0;
+            }
+            public void enqueue(int e)
+            {
+                Node newest= new Node (e,null)
+
+
+                if (isEmpty())
+                    front = newest;
+                else
+                    rear.next = newest;
+                rear = newest;
+                size = size + 1;
+                
+            }
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
--- dsapratice/Stack.cs
+++ dsapratice/Stack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -90,3 +90,4 @@ namespace dsapratice
         }
     }
 }
+*/
